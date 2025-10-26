bb7f6fb22b5d75c98f381c69785688edbcb8af17
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2025-10-26 18:06:34 +0100

    fix script for github action

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 FILENAME=Todo_$(date +%F_%H-%M).gz
 
-docker exec -it mongodb mongodump --uri="mongodb://root:password@localhost:27017/Todo?authSource=admin" --gzip --archive=/tmp/$FILENAME
+docker exec mongodb mongodump --uri="mongodb://root:password@localhost:27017/Todo?authSource=admin" --gzip --archive=/tmp/$FILENAME
 docker cp mongodb:/tmp/$FILENAME .
 
 aws s3 cp $FILENAME s3://automated-db-backup/backups/ --endpoint-url https://dd131b94df837c658bbc7e7f035537a7.r2.cloudflarestorage.com
